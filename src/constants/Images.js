7813da9422e6c7b95b682d5683e8ada2903cb98f
Sebastian Rivera Soto <sebastianrs72@gmail.com>
2019-09-26 01:31:48 -0600

    Add new icons and fix layout (#1)
    * Fix file system issues
    
    * Change icons and design grid

---
+++ src/constants/Images.js
@@ -0,0 +1,10 @@
+const images = {
+  BathIcons: {
+      soap:         require('../assets/images/shampoo.png'),
+      alcohol:      require('../assets/images/toothpaste.png'),
+      toiletPaper:  require('../assets/images/toilet-paper.png'),
+      toilet:       require('../assets/images/toilet.png'),
+  },
+};
+
+export default images;
